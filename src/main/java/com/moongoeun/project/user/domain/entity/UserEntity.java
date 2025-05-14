5299156a7d1aea6c67e9ba169d37c1dc94b960cd
문고은 <>
2025-05-14 18:12:51 +0900

    feat: 사용자 Entity 및 권한 타입 Enum 생성

---
+++ src/main/java/com/moongoeun/project/user/domain/entity/UserEntity.java
@@ -0,0 +1,44 @@
+package com.moongoeun.project.user.domain.entity;
+
+import com.moongoeun.project.user.domain.vo.RoleType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserEntity {
+    private Long id;
+    private String username;
+    private String password;
+    private String nickname;
+    private RoleType[] roles;
+
+    @Builder
+    private UserEntity(
+        String username,
+        String password,
+        String nickname,
+        RoleType[] roles
+    ) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.roles = roles;
+    }
+
+    public static UserEntity of(
+        String username,
+        String password,
+        String nickname,
+        RoleType[] roles
+    ) {
+        return UserEntity.builder()
+            .username(username)
+            .password(password)
+            .nickname(nickname)
+            .roles(roles)
+            .build();
+    }
+}
