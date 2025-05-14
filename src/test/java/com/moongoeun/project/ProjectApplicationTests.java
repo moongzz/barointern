7e0a7fb0d589a93c61b585e4890edd38a56552d3
문고은 <>
2025-05-14 13:45:30 +0900

    Initial commit

---
+++ src/test/java/com/moongoeun/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moongoeun.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
