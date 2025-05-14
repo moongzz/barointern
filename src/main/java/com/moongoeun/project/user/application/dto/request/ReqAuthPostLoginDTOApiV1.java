965aff4e3f86a64e106eb3f3256f8915b134f895
문고은 <>
2025-05-15 02:35:44 +0900

    fix: DTO 역직렬화를 위한 생성자 추가

--- src/main/java/com/moongoeun/project/user/application/dto/request/ReqAuthPostLoginDTOApiV1.java
+++ src/main/java/com/moongoeun/project/user/application/dto/request/ReqAuthPostLoginDTOApiV1.java
@@ -3,19 +3,24 @@ package com.moongoeun.project.user.application.dto.request;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
-@Builder
 public class ReqAuthPostLoginDTOApiV1 {
+
     @Valid
     @NotNull(message = "회원 정보를 입력해주세요.")
     private User user;
 
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Getter
-    @Builder
     public static class User {
+
         @NotBlank(message = "아이디를 입력해주세요.")
         private String username;
         @NotBlank(message = "비밀번호를 입력해주세요")
