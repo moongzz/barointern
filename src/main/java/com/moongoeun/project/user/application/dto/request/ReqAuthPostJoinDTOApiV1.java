1c8a0f2944c340b1be246f1776c4ddd26666a1d9
문고은 <>
2025-05-14 18:57:59 +0900

    feat: 아이디와 닉네임 길이 제한을 20자에서 30자로 수정

--- src/main/java/com/moongoeun/project/user/application/dto/request/ReqAuthPostJoinDTOApiV1.java
+++ src/main/java/com/moongoeun/project/user/application/dto/request/ReqAuthPostJoinDTOApiV1.java
@@ -18,7 +18,7 @@ public class ReqAuthPostJoinDTOApiV1 {
     @Builder
     public static class User {
         @NotBlank(message = "아이디를 입력해주세요.")
-        @Size(max = 20, message = "아이디는 20자까지 가능합니다.")
+        @Size(max = 30, message = "아이디는 30자까지 가능합니다.")
         private String username;
 
         @NotBlank(message = "비밀번호를 입력해주세요.")
@@ -26,7 +26,7 @@ public class ReqAuthPostJoinDTOApiV1 {
         private String password;
 
         @NotBlank(message = "닉네임을 입력해주세요")
-        @Size(max = 20, message = "닉네임은 20자까지 가능합니다.")
+        @Size(max = 30, message = "닉네임은 30자까지 가능합니다.")
         private String nickname;
     }
 }
